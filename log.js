f291832c334d80101e2bdffcbc244d452ea0609e
Doriman75 <pierantonio.manco@gmail.com>
2018-07-23 16:52:35 +0200

    first release

---
+++ log.js
@@ -0,0 +1,19 @@
+const request = require("request-promise-native");
+const commander = require('commander')
+  .version("0.0.1")
+  .usage('[options]')
+  .option("-u, --url <url>", "url of the queue of redis-mb.")
+  .parse(process.argv);
+
+if (!commander.url) {
+  console.error("url is mandatory")
+  process.exit(1);
+}
+async function consume() {
+  while (true) {
+    var response = await request(commander.url);
+    if (response) console.log(response);
+  }
+}
+
+consume();
