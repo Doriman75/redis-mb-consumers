1972aad23acbc9487520a99f02e8b564ba948861
Doriman75 <pierantonio.manco@gmail.com>
2018-07-26 11:57:52 +0200

    ok

--- log.js
+++ log.js
@@ -3,21 +3,29 @@ const commander = require('commander')
   .version("0.0.1")
   .usage('[options]')
   .option("-u, --url <url>", "url of the queue of redis-mb.")
+  .option("-s, --speed", "trace number of messages per second")
   .parse(process.argv);
 
 if (!commander.url) {
   console.error("url is mandatory")
   process.exit(1);
 }
+var n = 0;
 async function consume() {
   while (true) {
     try {
       var response = await request(commander.url);
-      if (response) console.log(response);
+      if (response) n++;
+      if (response && !commander.speed) console.log(response);
     } catch (e) {
       console.error(e);
     }
   }
 }
+
+setInterval(() => {
+  console.log(n, "messages per second");
+  n = 0;
+}, 1000);
 
 consume();
